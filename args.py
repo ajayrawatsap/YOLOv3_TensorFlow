969b4dbef538257da1d10fe03433bef746ca122d
ajayrawatsap <37475177+ajayrawatsap@users.noreply.github.com>
2019-11-02 09:59:48 +0530

    update params

--- args.py
+++ args.py
@@ -13,8 +13,8 @@ restore_path = './data/darknet_weights/yolov3.ckpt'  # The path of the weights t
 save_dir = './checkpoint/'  # The directory of the weights to save.
 log_dir = './data/logs/'  # The directory to store the tensorboard log files.
 progress_log_path = './data/progress.log'  # The path to record the training progress.
-anchor_path = './data/yolo_anchors.txt'  # The path of the anchor txt file.
-class_name_path = './data/coco.names'  # The path of the class names.
+anchor_path = './data/my_data/anchors.txt'  # The path of the anchor txt file.
+class_name_path = './data/my_data/data.names'  # The path of the class names.
 
 ### Training releated numbers
 batch_size = 6
